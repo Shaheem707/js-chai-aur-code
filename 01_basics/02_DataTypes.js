16ef0e824a12cfacf1b141caa8d1eaed84be390d
Shaheem707 <141420269+Shaheem707@users.noreply.github.com>
2024-09-13 05:30:55 +0000

    DataTypes Lecture

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS code as the newer version
+
+// alert("hello");  - Cant be used in NodeJS, used in browser.
+
+// Code readablity should be high.
+
+// Recommended documentation is MDN, but original is tc39.es(search on web)
+
+/* Primitive DataTypes 
+Number - 2 to power 53 
+  -  For larger numbers big int is used.
+String - ""
+Boolean - true/false
+Null - Standalone Value
+Undefined - Not Defined
+Symbol - Used for Uniqueness */
+
+console.log(typeof null) // Type of null is basically object.
+console.log(typeof undefined); // Type of undefined is undefined itself.
+
