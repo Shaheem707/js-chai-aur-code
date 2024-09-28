a92decebe9fe8d28b78e947499acb15cb08f6450
Shaheem707 <141420269+Shaheem707@users.noreply.github.com>
2024-09-28 10:55:27 +0000

    Strings, Maths and Num

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,5 @@
+const URL = "https://shaheem.com/shaheem%20malik";
+
+console.log(URL.replace('%20', '-'));
+ 
+// Study the available methods from MDN Docs.
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,18 @@
+const Score = 400;
+// console.log(Score);
+
+const Balance = new Number(100);
+// console.log(Balance);
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+
+// -------------------------- Maths ------------------------
+
+// console.log(Math.floor(Math.random()*10 + 1));
+
+// for random value between a range:
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
